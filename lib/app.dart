d121a02a006f4bec8c51576d67a336bffe9a78bb
Anujit-Datta <anujitbestbd@gmail.com>
2024-05-04 03:46:45 +0600

    ...

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,16 @@
+import 'package:ecommerce/presentation/screens/splash_screen.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
 
 class ECommerce extends StatelessWidget {
   const ECommerce({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const GetMaterialApp(
+      title: 'E-Commerce',
+      home: SplashScreen(),
+    );
   }
 }
