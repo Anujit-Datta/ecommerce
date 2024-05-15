9cc75deb5a5dee8c950b8a9cfdb64780b0a7bf4b
Anujit-Datta-mac <anujitbestbd@gmail.com>
2024-05-16 03:49:00 +0600

    i...

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/presentation/utils/app_colors.dart';
 import 'package:ecommerce/presentation/utils/asset_paths.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -14,18 +15,72 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    Size sizes=MediaQuery.of(context).size;
+    Size sizes = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
         automaticallyImplyLeading: false,
         title: Logo(
           path: AssetPaths.navBarLogo,
-          scale: sizes.height*0.04,
+          scale: sizes.height * 0.045,
         ),
         actions: [
-          Logo(scale: 20, path: AssetPaths.appLogo)
+          TopBarIconButtons(
+            sizes: sizes,
+            selection: 'person',
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          TopBarIconButtons(
+            sizes: sizes,
+            selection: 'dialer',
+          ),
+          SizedBox(
+            width: sizes.height * 0.015,
+          ),
+          TopBarIconButtons(
+            sizes: sizes,
+            selection: 'bell',
+          ),
+          const SizedBox(
+            width: 10,
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class TopBarIconButtons extends StatelessWidget {
+  const TopBarIconButtons({
+    super.key,
+    required this.sizes,
+    required this.selection,
+  });
+
+  final Size sizes;
+  final String selection;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      customBorder: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50),
+      ),
+      splashColor: AppColors.primaryColor.withOpacity(0.2),
+      child: CircleAvatar(
+        radius: sizes.height * 0.023,
+        backgroundColor: Colors.grey.withOpacity(0.25),
+        child: Logo(
+          scale: selection=='bell'?sizes.height*0.045:sizes.height * 0.023,
+          path: selection == 'person'
+              ? AssetPaths.navBarPersonLogo
+              : selection == 'dialer'
+                  ? AssetPaths.navBarDialerLogo
+                  : AssetPaths.navBarBellLogo,
+        ),
       ),
     );
   }
--- lib/presentation/utils/asset_paths.dart
+++ lib/presentation/utils/asset_paths.dart
@@ -1,9 +1,14 @@
 class AssetPaths{
   static const _basePath='assets';
   static const _imagesPath='$_basePath/images';
+  static const _topbarPath='$_imagesPath/topbar';
 
   static const appLogo = '$_imagesPath/logo.svg';
-  static const navBarLogo = '$_imagesPath/logo_nav.svg';
+
+  static const navBarLogo = '$_topbarPath/logo_nav.svg';
+  static const navBarPersonLogo = '$_topbarPath/person.svg';
+  static const navBarBellLogo = '$_topbarPath/bell.svg';
+  static const navBarDialerLogo = '$_topbarPath/dialer.svg';
 
 
 }
